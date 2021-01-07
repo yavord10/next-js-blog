164998dccf2458a130290b57f58d05bd1167fe59
yavord10 <yavor.v.dimitrov@gmail.com>
2021-01-07 11:26:03 +0800

    finish sample blog

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
 
 export const getStaticProps = async () => {
   const allPostsData = getSortedPostsData();
@@ -33,11 +35,13 @@ const Home = ({ allPostsData }) => {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,12 @@
 import Layout from '../../components/layout';
+import Date from '../../components/date';
+import Head from 'next/head';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
+import utilStyles from '../../styles/utils.module.css';
+
 export const getStaticProps = async ({ params }) => {
-    const postData = getPostData(params.id);
+    const postData = await getPostData(params.id);
     return {
         props: {
             postData
@@ -21,11 +25,16 @@ export const getStaticPaths = async () => {
 const Post = ({ postData }) => {
     return (
         <Layout>
-            {postData.title}
-            <br />
-            {postData.id}
-            <br />
-            {postData.date}
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
         </Layout>
     )
 };
